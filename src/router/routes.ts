6fda948e2db52104296fa13b79fbe7d824eb4dbc
Pedro Arthur Rodrigues Lopes Silva <pedro.rodrigues@dtidigital.com.br>
2023-04-20 11:01:24 -0300

    Removing unused packages

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,10 +5,8 @@ const routes: RouteRecordRaw[] = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('src/pages/VueEcharts.vue') }, 
-      { path: '/quasarTree', component: () => import('pages/IndexPage.vue') },
-      { path: '/vueOrgChart', component: () => import('pages/VueOrgChart.vue') },
-      { path: '/vOrgTree', component: () => import('pages/VOrgTree.vue') }    ],
+      { path: '', component: () => import('pages/IndexPage.vue') }
+    ],
   },
 
   // Always leave this as last one,
